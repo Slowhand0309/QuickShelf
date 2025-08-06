f4b229095893b575090dc7ae4b2e111694b58259
Slowhand0309 <slowhand0309@gmail.com>
2025-08-07 06:11:17 +0900

    🔧 Change Directory label to Folder in ContentView

--- QuickShelf/ContentView.swift
+++ QuickShelf/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
 
     var body: some View {
         VStack(alignment: .leading) {
-            Text("Directory")
+            Text("Folder")
             HStack {
                 TextField("Please select directory", text: $inputDir)
                     .disabled(true)
